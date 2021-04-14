513599fa08c2a9ab02990cae2fe37a4ebe0a7072
bhohnco <bryanhohn13@gmail.com>
2021-04-14 14:13:58 -0600

    Constructor and two methods add

--- src/pantry.js
+++ src/pantry.js
@@ -1,6 +1,28 @@
+import ingredientsData from "./data/ingredients";
+
 class Pantry {
   constructor(userIngredients) {
     this.contents = userIngredients;
+  }
+
+  getPantryStock() {
+    return this.contents;
+  }
+
+  getRequiredIngredients(recipe) {
+    return recipe.ingredients.map(ingredient => {
+      const ingredientList = {};
+      const id = ingredient.id;
+      const amount = ingredient.quantity.amount;
+      ingredientsData.forEach(ingredientData => {
+        const name = ingredientData.name;
+        if (ingredientData.id === id) {
+          ingredientList.name = name;
+          ingredientList.amount = amount;
+        }
+      })
+      return ingredientList;
+    })
   }
 }
 

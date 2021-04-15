e4a179cb6f01c242863d3993ac04b131ac6967a4
Your Name <stoner.michann@gmail.com>
2021-04-14 21:41:08 -0600

    Write test to search by tag and implementation code

--- src/recipeRepository.js
+++ src/recipeRepository.js
@@ -1,15 +1,35 @@
 class RecipeRepository {
-  constructor(data) {
-    this.recipes = data;
+  constructor(recipeData) {
+    this.recipes = recipeData;
+  }
+
+  returnTags(recipeTag) {
+    const taggedRecipes = [];
+    this.recipes.filter(recipe => {
+      if (recipe.tags.includes(recipeTag)) {
+        taggedRecipes.push(recipe);
+      }
+    });
+    return taggedRecipes;
   }
 
   findRecipe(searchText) {
-    return this.recipes.filter(recipe => {
-      return recipe.ingredients.find(ingredient => {
-        return (ingredient.name.includes(searchText)) ||
-        (recipe.name.includes(searchText))
-      });
+    const filteredRecipes = this.recipes.filter(recipe => {
+      const ingredientKeys = Object.keys(recipe.ingredients);
+      if (recipe.ingredients.name.includes(searchText) || recipe.name.includes(searchText)) {
+      }
+   
     })
+    // console.log(filteredRecipes);
+    return filteredRecipes;
+  
+    // const filteredRecipes = this.recipes.filter(recipe => {
+    //   return recipe.ingredients.find(ingredient => {
+    //     return (ingredient.name.includes(searchText)) ||
+    //     (recipe.name.includes(searchText))
+    //   });
+    // })
+    // console.log(filteredRecipes);
   }
 }
 

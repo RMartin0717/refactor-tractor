c00bd58323d7a33b8aece771c0be91c320194140
Riley Martin <riley.willow@gmail.com>
2021-04-17 19:14:43 -0600

    Update User Class testing to use separated testing data

---
+++ test/user-testing-data.js
@@ -0,0 +1,206 @@
+const userTestingData = [
+  {
+    "id": 1,
+    "name": "Saige O'Kon",
+    "pantry": [
+      {
+        "ingredient": 11477,
+        "amount": 1
+      },
+      {
+        "ingredient": 93820,
+        "amount": 1
+      },
+      {
+        "ingredient": 11297,
+        "amount": 3
+      },
+      {
+        "ingredient": 11547,
+        "amount": 5
+      },
+      {
+        "ingredient": 1082047,
+        "amount": 5
+      },
+      {
+        "ingredient": 1032050,
+        "amount": 1
+      },
+      {
+        "ingredient": 20081,
+        "amount": 4
+      },
+      {
+        "ingredient": 11215,
+        "amount": 2
+      },
+      {
+        "ingredient": 10514037,
+        "amount": 2
+      },
+      {
+        "ingredient": 2047,
+        "amount": 2
+      },
+      {
+        "ingredient": 12179,
+        "amount": 1
+      },
+      {
+        "ingredient": 1123,
+        "amount": 4
+      },
+      {
+        "ingredient": 11282,
+        "amount": 5
+      },
+      {
+        "ingredient": 9016,
+        "amount": 4
+      },
+      {
+        "ingredient": 14003,
+        "amount": 4
+      },
+      {
+        "ingredient": 6194,
+        "amount": 2
+      },
+      {
+        "ingredient": 16112,
+        "amount": 1
+      },
+      {
+        "ingredient": 6172,
+        "amount": 3
+      },
+      {
+        "ingredient": 2044,
+        "amount": 2
+      },
+      {
+        "ingredient": 2050,
+        "amount": 3
+      },
+      {
+        "ingredient": 1032009,
+        "amount": 2
+      },
+      {
+        "ingredient": 5114,
+        "amount": 4
+      },
+      {
+        "ingredient": 11282,
+        "amount": 2
+      },
+      {
+        "ingredient": 9412,
+        "amount": 4
+      },
+      {
+        "ingredient": 1017,
+        "amount": 2
+      },
+      {
+        "ingredient": 1056,
+        "amount": 1
+      },
+      {
+        "ingredient": 10220052,
+        "amount": 5
+      },
+      {
+        "ingredient": 2024,
+        "amount": 4
+      },
+      {
+        "ingredient": 1006972,
+        "amount": 4
+      },
+      {
+        "ingredient": 11124,
+        "amount": 1
+      },
+      {
+        "ingredient": 18371,
+        "amount": 5
+      },
+      {
+        "ingredient": 18371,
+        "amount": 4
+      },
+      {
+        "ingredient": 1001,
+        "amount": 4
+      },
+      {
+        "ingredient": 1082047,
+        "amount": 4
+      },
+      {
+        "ingredient": 11215,
+        "amount": 4
+      },
+      {
+        "ingredient": 18064,
+        "amount": 3
+      },
+      {
+        "ingredient": 11291,
+        "amount": 3
+      },
+      {
+        "ingredient": 1082047,
+        "amount": 2
+      },
+      {
+        "ingredient": 1001,
+        "amount": 1
+      },
+      {
+        "ingredient": 1082047,
+        "amount": 1
+      },
+      {
+        "ingredient": 6172,
+        "amount": 2
+      },
+      {
+        "ingredient": 20081,
+        "amount": 2
+      },
+      {
+        "ingredient": 99223,
+        "amount": 4
+      },
+      {
+        "ingredient": 11547,
+        "amount": 1
+      },
+      {
+        "ingredient": 10118368,
+        "amount": 4
+      },
+      {
+        "ingredient": 18350,
+        "amount": 3
+      },
+      {
+        "ingredient": 10018079,
+        "amount": 4
+      },
+      {
+        "ingredient": 18371,
+        "amount": 3
+      },
+      {
+        "ingredient": 1230,
+        "amount": 3
+      },
+    ]
+  }
+];
+
+module.exports = userTestingData;
